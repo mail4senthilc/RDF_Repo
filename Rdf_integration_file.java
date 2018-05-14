9b84d62c99aae25460b8238a650fd69341d59a33
mail4senthilc <gotosenthil@rediffmail.com>
2018-05-14 22:58:53 +0530

    Stride Testing

--- Rdf_integration_file.java
+++ Rdf_integration_file.java
@@ -4,6 +4,6 @@
 public class HelloHCL
 {
 	public static void main(String[] args) {
-		System.out.println("Hello HCL Technologies Ltd, Chennai !");
+		System.out.println("Hello HCL Technologies Ltd, Chennai, TamilNadu, India !");
 	}
 }
